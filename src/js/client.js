b2dff31f29a0a057da7b7ad6f11eeb08bb3908bb
Unknown <jua16073@uvg.edu.gt>
2018-07-16 13:17:36 -0600

    Colores, y turnos

--- src/js/client.js
+++ src/js/client.js
@@ -1,87 +1,121 @@
 const state = {
-    game : [2,2,2,2,2,2,2,2,2],
-    turno : 0,
+  game: [2, 2, 2, 2, 2, 2, 2, 2, 2],
+  turno: 0,
 };
 
 const render = btnState => {
 
-    if ((root.hasChildNodes())){
-        root.innerHTML = null;
-    }
-    const title = document.createElement('h1');
-    title.innerHTML = 'TOTITO';
-    root.appendChild(title);
+  if ((root.hasChildNodes())) {
+    root.innerHTML = null;
+  }
 
-    const grid = document.createElement('div');
-    grid.className = 'grid';
-    root.appendChild(grid);
+  // Titulo TOTITO
+  const title = document.createElement('h1');
+  title.innerHTML = 'TOTITO';
+  title.className = 'title';
+  root.appendChild(title);
 
-    const row1 = document.createElement('div');
-    row1.className = 'row';
-    grid.appendChild(row1);
+  // Grid que contiene a las filas con los botones para jugar
+  const grid = document.createElement('div');
+  grid.className = 'grid';
+  root.appendChild(grid);
 
-    const row2 = document.createElement('div');
-    row2.className = 'row';
-    grid.appendChild(row2);
+  // Filas para los botones
+  const row1 = document.createElement('div');
+  row1.className = 'row';
+  grid.appendChild(row1);
 
-    const row3 = document.createElement('div');
-    row3.className = 'row';
-    grid.appendChild(row3);
+  const row2 = document.createElement('div');
+  row2.className = 'row';
+  grid.appendChild(row2);
 
-    const redo = document.createElement('button');
-    redo.className = 'redo';
-    redo.innerHTML = 'REINICIO';
-    root.appendChild(redo);
-    
-    let id = 0;
-    const btns = btnState.game.map(
-        (position, i) => {
-            const btn = document.createElement('button');
-            btn.className = 'btns';
-            btn.id = id;
+  const row3 = document.createElement('div');
+  row3.className = 'row';
+  grid.appendChild(row3);
 
-            if (position === 1){
-                btn.classList.add('pressedx');
-                btn.innerHTML = '<img src = "assets/X.png" disabled>';
-            }
+  // Div en el que se encuentran los botones de reinicio 
+  // y a que jugador le toca
+  const divf = document.createElement('div');
+  divf.className = 'final';
+  root.appendChild(divf);
 
-            if (position === 0){
-                btn.classList.add('pressedo');
-                btn.innerHTML = '<img src = "assets/O.png">';
-            }
+  const texto = document.createElement('p');
+  texto.className = 'texto';
+  texto.innerHTML = 'Turno de';
+  divf.appendChild(texto);
 
-            id++;
+  const image = document.createElement('img');
+  image.className = 'img';
 
-            btn.onclick = (self) => {
-                btnState.game[btn.id]= (btnState.turno + 1) % 2;
-                btnState.turno = (btnState.turno + 1) % 2;
-                render(btnState);
-            };
-            
-            return btn;
-        }
-    );
 
-    let x = 0;
-    btns.forEach(
-        btn => {
-            if (x<3){
-                row1.appendChild(btn);
-            }
-            else if(x<6){
-                row2.appendChild(btn);
-            }
-            else if (x<9){
-                row3.appendChild(btn);
-            }
-            x++;
-            }
-    );
+  // ifs para 
+  if(btnState.turno == 0){
+      image.innerHTML = '<img src = "assets/X.png">';
+  }
 
-    redo.onclick = () => {
-        btnState.game = [2,2,2,2,2,2,2,2,2];
+  if(btnState.turno == 1){
+    image.innerHTML = '<img src = "assets/O.png">';
+  }
+
+  divf.appendChild(image);
+
+  const redo = document.createElement('button');
+  redo.className = 'button';
+  redo.innerHTML = 'REINICIO';
+  divf.appendChild(redo);
+
+  let id = 0;
+  const btns = btnState.game.map(
+    (position, i) => {
+      const btn = document.createElement('button');
+      btn.className = 'btns';
+      btn.id = id;
+
+      if (position === 1) {
+        btn.classList.add('pressed');
+        btn.innerHTML = '<img src = "assets/X.png" disabled>';
+        btn.disabled = true;
+      }
+
+      if (position === 0) {
+        btn.classList.add('pressed');
+        btn.innerHTML = '<img src = "assets/O.png">';
+        btn.disabled = true;
+      }
+
+      id++;
+
+      btn.onclick = (self) => {
+        btnState.game[btn.id] = (btnState.turno + 1) % 2;
+        btnState.turno = (btnState.turno + 1) % 2;
         render(btnState);
+      };
+
+      return btn;
     }
+  );
+
+  let x = 0;
+  btns.forEach(
+    btn => {
+      if (x < 3) {
+        row1.appendChild(btn);
+      }
+      else if (x < 6) {
+        row2.appendChild(btn);
+      }
+      else if (x < 9) {
+        row3.appendChild(btn);
+      }
+      x++;
+    }
+  );
+
+  redo.onclick = () => {
+    btnState.game = [2, 2, 2, 2, 2, 2, 2, 2, 2];
+    render(btnState);
+  }
+  
 };
 
 render(state);
